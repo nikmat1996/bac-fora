5250914b1a9e1b03307e0b69bc27e2c76709558b
nikmat1996 <nikhilmathew1996@gmail.com>
2024-05-23 12:42:24 +0530

    body black

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Poppins } from 'next/font/google'
 import './globals.css'
 import { Toaster } from "@/components/ui/toaster"
+import { twMerge } from 'tailwind-merge'
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -15,14 +16,14 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='en'>
-      <head>
+    <html lang='en' >
+      <head >
         <link rel="icon" href="/images/favicon.ico" sizes="any" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon_io/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon_io/favicon-16x16.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/favicon_io/apple-touch-icon.png" />
       </head>
-      <body className={poppins.className}>
+      <body className={twMerge(poppins.className, "bg-black")}>
         
         {children}
         <Toaster />
